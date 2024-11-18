393497655b469a6778324d1108ff264a6a28e8e6
Felix Wenzel <62264190+wenzel-felix@users.noreply.github.com>
2024-11-18 21:49:44 +0100

    Update ubuntu2004_setup.sh

--- ubuntu2004_setup.sh
+++ ubuntu2004_setup.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> $HOME/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 git clone https://github.com/NVIDIA/k8s-dra-driver.git
 cd k8s-dra-driver
 
