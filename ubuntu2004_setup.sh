f2ecb60e311b84c1382145f31b6fcdcbc484a191
Felix Wenzel <62264190+wenzel-felix@users.noreply.github.com>
2024-11-18 21:04:32 +0100

    Update ubuntu2004_setup.sh

--- ubuntu2004_setup.sh
+++ ubuntu2004_setup.sh
@@ -57,10 +57,10 @@ nvidia-smi -L
 git clone https://github.com/NVIDIA/k8s-dra-driver.git
 cd k8s-dra-driver
 
-sed -i 's/\bdocker\b/sudo docker/g' ./demo/clusters/kind/create-cluster.sh
+find ./demo/clusters/kind -type f -print0 | xargs -0 sed -i 's/\bdocker\b/sudo docker/g'
+
 ./demo/clusters/kind/create-cluster.sh
 
-sed -i 's/\bdocker\b/sudo docker/g' ./demo/clusters/kind/build-dra-driver.sh
 ./demo/clusters/kind/build-dra-driver.sh
 
 ./demo/clusters/kind/install-dra-driver.sh
